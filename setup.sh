ad9e4762d4551342afef30ecfc04304d641abb73
Pedro Henrique Viana dos Santos <pedrohenry.viana@gmail.com>
2022-03-30 22:22:57 -0300

    [CHORE]: add others config for docker

--- setup.sh
+++ setup.sh
@@ -7,9 +7,10 @@ then
   DIR="/usr/bin/docker-compose"
   if [[ ! -e $DIR ]]; then
     echo "Installing Docker..."
-    sudo amazon-linux-extras install docker -y
+    sudo yum install docker -y
     sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    sudo chkconfig docker on
     sudo service docker start
     sudo usermod -a -G docker ec2-user
     sudo chmod 666 /var/run/docker.sock
